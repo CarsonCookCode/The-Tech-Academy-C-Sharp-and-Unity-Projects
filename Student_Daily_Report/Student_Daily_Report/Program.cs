f56007fd857bbb64e27db32e1eb25ce2f90b3206
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-11-28 14:26:08 -0700

    Added Student_Daily_Report

---
+++ Student_Daily_Report/Student_Daily_Report/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+
+        Console.WriteLine("What is your name?");
+        string studentName = Console.ReadLine();
+        Console.WriteLine("What course are you on?");
+        string courseName = Console.ReadLine();
+        Console.WriteLine("What page number?");
+        string pageNumberAnswer = Console.ReadLine();
+        int pageNumber = Convert.ToInt32(pageNumberAnswer);
+        Console.WriteLine("Do you need help with anything? Please answer \u201Dtrue\u201D or \u201Dfalse.\u201D");
+        string needHelpAnswer = Console.ReadLine();
+        bool needHelp = Convert.ToBoolean(needHelpAnswer);
+        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+        string positiveExperiences = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        string feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        string hoursStudiedAnswer = Console.ReadLine();
+        decimal hoursStudied = Convert.ToDecimal(hoursStudiedAnswer);
+
+        Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        Console.Read();
+
+
+
+    }
+}
