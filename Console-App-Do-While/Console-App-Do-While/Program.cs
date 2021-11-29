5ee9307be2043183d9bc56fc8be678a9e9e3e997
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-11-29 12:26:12 -0700

    Added Console_App_Do_While

---
+++ Console-App-Do-While/Console-App-Do-While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Console_App_Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            while (num1 != 15)
+            {
+                num1++;
+                Console.WriteLine("This will stop at 15: " + num1);
+            }
+            Console.Read();
+
+            int num2 = 0;
+            do
+            {
+                Console.WriteLine("This will stop at 10: " + num2);
+                num2++;
+            } while (num2 <= 10);
+        }
+    }
+}
---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1 = "child";
+            switch (num1)
+            {
+                case "asdf":
+                    Console.WriteLine("hi");
+                    break;
+                case "child":
+                    Console.WriteLine("I don't exist.");
+                    break;
+                case "fdsa":
+                    Console.WriteLine("yep");
+                    break;
+            }
+        }
+    }
+}
