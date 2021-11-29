ae5c7e5576e6d5e437bfaa3cf8213ee8e48d1857
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-11-29 11:54:30 -0700

    Added Branching.

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("How many pounds does your package weigh?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+            Console.WriteLine("How many inches wide is your package?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many inches tall is your package?");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many inches long is your package?");
+            int length = Convert.ToInt32(Console.ReadLine());
+            if (width + length + height > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.Read();
+                Environment.Exit(0);
+            }
+            int quote = width * length * height * weight / 100;
+
+            Console.WriteLine("Package shipping cost: $" + quote + ".00");
+            Console.Read();
+        }
+    }
+}
---
+++ ElseStatements/ElseStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ElseStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 12;
+            //int num2 = 12;
+            //int num3 = 100;
+
+            //if (num1 < num2)
+            //{
+            //    Console.WriteLine("1");
+            //}
+            //else if (num2 < num1)
+            //{
+            //    Console.WriteLine("2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(num3);
+            //}
+
+            int num1 = 5;
+            int num2 = 10;
+
+            string answer = num1 > num2 ? "true" : "false";
+            Console.WriteLine(answer);
+            
+        }
+    }
+}
