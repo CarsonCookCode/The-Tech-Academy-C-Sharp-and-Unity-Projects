b464d42a711bf86c94e48bbee9e1a7602a6b240a
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-13 10:45:05 -0700

    Added MethodPractice.

---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass newClass = new myClass();
+
+            Console.WriteLine("Enter an integer...");
+            int parameter = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(parameter + " + 5 = " + newClass.add5(parameter));
+            Console.WriteLine(parameter + " - 5 = " + newClass.subtract5(parameter));
+            Console.WriteLine(parameter + " * 5 = " + newClass.multiplyBy5(parameter));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodPractice/MethodPractice/myClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodPractice
+{
+    class myClass
+    {
+        public int add5(int x)
+        {
+            return (x + 5);
+        }
+
+        public int subtract5(int x)
+        {
+            return (x - 5);
+        }
+
+        public int multiplyBy5(int x)
+        {
+            return (x * 5);
+        }
+
+    }
+}
