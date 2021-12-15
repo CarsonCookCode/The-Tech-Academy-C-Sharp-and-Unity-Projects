3a22eddf2fa5a98740e208149b48503aa154bde6
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-15 16:10:23 -0700

    Added MainMethodPractice

---
+++ MainMethodPractice/MainMethodPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MainMethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass myObject = new myClass();
+            int answer1 = myObject.method1(5);
+            int answer2 = myObject.method1(5.0m);
+            int answer3 = myObject.method1("5");
+
+
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodPractice/MainMethodPractice/myClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethodPractice
+{
+    class myClass
+    {
+        public int method1 (int x)
+        {
+            return x + 1;
+        }
+
+        public int method1(decimal x)
+        {
+            return Convert.ToInt32(x) + 2;
+        }
+
+        public int method1(string x)
+        {
+            return Convert.ToInt32(x) + 3;
+        }
+    }
+}
