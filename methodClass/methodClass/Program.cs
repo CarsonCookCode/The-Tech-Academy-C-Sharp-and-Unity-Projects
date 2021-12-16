533b94bcce4014ac8c1f41cbd3e9705c1351bfd5
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-15 17:36:36 -0700

    Added methodClass

---
+++ methodClass/methodClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace methodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass myObject = new myClass();
+
+            myObject.method1(15, 50);
+
+            myObject.method1(x: 200, y: 300);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodClass/methodClass/myClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodClass
+{
+    class myClass
+    {
+        public void method1 (int x, int y)
+        {
+            x += 100;
+            Console.WriteLine(y);
+        }
+    }
+}
