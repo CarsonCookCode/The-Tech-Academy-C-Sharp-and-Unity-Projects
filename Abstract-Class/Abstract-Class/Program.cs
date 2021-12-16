de0a509084183a937d497ae94b1cd921f6cb4ca5
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-15 19:40:03 -0700

    Added Abstract-Class

---
+++ Abstract-Class/Abstract-Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee() {FirstName = "Sample", LastName = "Student" };
+
+            myEmployee.SayName();
+
+            Console.ReadLine();
+
+        } 
+    }
+}
