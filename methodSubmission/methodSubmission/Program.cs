06bb5198da8d4fc6d199fa59c6098b6cec091bde
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-15 17:26:38 -0700

    Added MainMethodPractice

---
+++ methodSubmission/methodSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace methodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            intClass myObject = new intClass();
+
+
+            Console.WriteLine("Input two numbers, one at a time. Second number is optional...");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(myObject.method1(x, y));
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine(myObject.method1(x));
+            }
+
+            Console.ReadLine();
+
+        }
+       
+    }
+}
---
+++ methodSubmission/methodSubmission/intClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodSubmission
+{
+    class intClass
+    {
+        public int method1(int x, int y = 10)
+        {
+            int z = x + y;
+            return z;
+        }
+    }
+}
