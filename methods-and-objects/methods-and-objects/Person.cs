a89deadc6f14a611e0f4f1cc79cd877a8040aef0
Carson Cook <73494842+CarsonCookCode@users.noreply.github.com>
2021-12-15 18:38:30 -0700

    Added methods-and-objects

---
+++ methods-and-objects/methods-and-objects/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods_and_objects
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ methods-and-objects/methods-and-objects/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace methods_and_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            myEmployee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
